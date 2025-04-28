6423e45e42d96df841c9e0845f00782fcba6c5c5
KhanhDuy <duyntk74105@gmail.com>
2025-04-28 18:19:29 +0700

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CD4_ApplyAgileProject
+## Front end setup
+### When new packages:
+    - npm install
+### Run debug:
+    - npm run dev
---
+++ front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-4xl text-primary">Hello Scrum Agile</div>
+    </>
+  )
+}
+
+export default App
